aeb3df687dd12db3275ba54444bda26a6fad9ba8
Dr-MiMI <olagundoyemiracle5@gmail.com>
2023-06-16 20:08:14 +0100

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/** 
+ * main - prints "Entry point"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%d", sizeof(int);
+	printf("%f", sizeof(float);
+	printf("%c", sizeof(char);
+	printf("%u", sizeof(double);
+	return (0);
+}
