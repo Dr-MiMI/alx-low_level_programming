ed2a675b4f9555dda3d058a579d0cf306c1e0bbb
Dr-MiMI <olagundoyemiracle5@gmail.com>
2023-06-21 23:59:23 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet
+ *
+ * Description: This function prints the alphabet from 'a' to 'z'
+ * Return: None
+ */
+
+void print_alphabet(void)
+{
+char n;
+for (n = 'a'; n <= 'z'; n++)
+{
+	_putchar(n);
+}
+	_putchar('\n');
+}
